b6899d6a0174c25bdaac3a9de52874ab68073b73
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-23 23:20:50 -0700

    chore: decide to use clojure for this project instead
    It will be a good learning experience! :)

--- algoapi_gen/csharp_templates/imports.py
+++
@@ -1,9 +0,0 @@
-imports = """\
-using System;
-using AlgoSdk;
-using AlgoSdk.Crypto;
-using AlgoSdk.LowLevel;
-using Unity.Collections;
-using UnityEngine;
-using static UnityEngine.Networking.UnityWebRequest;
-"""
--- algoapi_gen/open_api/models.py
+++
@@ -1,4 +0,0 @@
-class Models:
-
-    def __init__(self, schema: dict):
-        self.schema = schema
