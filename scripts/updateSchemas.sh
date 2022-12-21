68908b8bedf520db210087a2a3ff5d9e43a1aa84
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-12-20 19:28:31 -0800

    add updateSchemas action

--- scripts/updateSchemas.sh
+++ scripts/updateSchemas.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/daemon/algod/api/algod.oas2.json --output resources/algod/algod.oas2.json
 curl https://raw.githubusercontent.com/algorand/indexer/master/api/indexer.oas2.json --output resources/indexer/indexer.oas2.json
-curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/daemon/kmd/api/swagger.json --output resources/kmd/swagger.json
+# curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/daemon/kmd/api/swagger.json --output resources/kmd/swagger.json
